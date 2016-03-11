025b3171833732f81264d28c602dc687c1fae48d
Shubhi Jain <shubhijain@caltech.edu>
2016-03-11 01:39:31 -0800

    progress

--- find_people.py
+++ find_people.py
@@ -1,5 +1,6 @@
 import json
 import time
+import os
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -13,12 +14,55 @@ def read_config():
         config = json.load(f)
     return config
 
-def search_person(company, job):
-    driver = webdriver.Chrome()
-    driver.get("https://www.linkedin.com")
-    assert "Python" in driver.title
-    elem = driver.find_element_by_name("q")
-    elem.send_keys("pycon")
+def login(driver):
+    '''
+    Login to linkedin.
+    '''
+    driver.get('https://www.linkedin.com/')
+    elem = driver.find_element_by_id('login-email')
+    elem.send_keys(os.environ['EMAIL_USERNAME'])
+    elem = driver.find_element_by_id('login-password')
+    elem.send_keys(os.environ['EMAIL_PASSWORD'])
     elem.send_keys(Keys.RETURN)
-    assert "No results found." not in driver.page_source
+
+def search_person(driver, company, job):
+    '''
+    Opens a linkedin page and searches for a specific person with a specific job
+    and specific company.
+    '''
+    driver.get('https://www.linkedin.com/vsearch/f?f_N=F,S,A&openFacets=N,G,CC&rsid=1934857271457685716329&adv=open')
+    # Enter in title
+    elem = driver.find_element_by_name('title')
+    elem.send_keys(job)
+    # Select current in title advanced options
+    driver.find_element_by_xpath("//select[@name='titleScope']/option[text()='Current']").click()
+    # Enter in company name
+    elem = driver.find_element_by_name('company')
+    elem.send_keys(company)
+    # Select current in company advanced options
+    driver.find_element_by_xpath("//select[@name='companyScope']/option[text()='Current']").click()
+    # Submit
+    elem = driver.find_element_by_name('submit')
+    elem.click()
+    # Wait for results to load
+    time.sleep(2)
+    if 'Sorry, no results containing' in driver.page_source:
+        return None
+    else:
+        # TODO: Return list of names in search results
+        return ''
+
+
+def no_results(driver):
+    elem = driver.find_element_by_id('empty-results-description')
+
+if __name__ == '__main__':
+    driver = webdriver.Chrome()
+    login(driver)
+    # Sleep 5 seconds
+    time.sleep(5)
+    search_person(driver, 'Udacity', 'Public Relations')
+    time.sleep(3)
+    # TODO: Write results of search_person to a json file
+    # (key: company, value: [name])
     driver.close()
